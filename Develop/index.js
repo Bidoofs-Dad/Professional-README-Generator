1b32f01d4205f8ab7365daf561df908a5b0c5d34
Zane Werle <zanewerle@gmail.com>
2023-05-02 19:37:50 -0500

    copied over the questions, packages and a couple of functions from our week 9 mini-project, just to populate those spots, will update them with different questions etc.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,44 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "fullName",
+        message: "What is your first and last name?",
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Where do you live?",
+      },
+      {
+        type: "input",
+        name: "bio",
+        message: "Say a little bit about yourself!",
+      },
+      {
+        type: "input",
+        name: "linkedInURL",
+        message: "Include your a link to your LinkedIn!",
+      },
+      {
+          type: "input",
+          name: "gitHubURL",
+          message: "Include your a link to your GitHub!",
+        }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("portfolio.md", portfolioRequested, (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
